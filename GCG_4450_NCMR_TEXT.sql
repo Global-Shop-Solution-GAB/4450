f9039999ac8c865c04fe3aae96b9464309102bcc
Service Web <sw@gssmail.com>
2024-03-12 08:59:21 -0500

    Commit changes in C:\Websites\Git\emarlow\4450\GCG_4450_NCMR_TEXT.sql

---
+++ GCG_4450_NCMR_TEXT.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_4450_NCMR_TEXT
+ * ========================================== */
+CREATE TABLE "GCG_4450_NCMR_TEXT"(
+ "PURCHASE_ORDER" CHAR(7),
+ "PO_LINE" CHAR(4),
+ "RECEIVER" CHAR(6),
+ "CONTROL_NUMBER" CHAR(7),
+ "TEXT" LONGVARCHAR);
+
+
